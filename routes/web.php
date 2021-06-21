41cd512c6b4616077311808666241ce85577560b
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-21 19:34:49 +0200

    modifico rotta con array comics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $datiComics = config('comics');
+
+    $datiView = [
+        'comicsList' => $datiComics
+    ];
+
     return view('home');
 });
