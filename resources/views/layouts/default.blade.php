ed7a4d676e70660bce8ba6267588fd7a523f8499
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-21 19:27:24 +0200

    importo laravel.php in config

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,6 +14,10 @@
     <script src="https://kit.fontawesome.com/010b527a19.js" crossorigin="anonymous"></script>
 
 
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    
+
     {{-- Css --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
@@ -21,6 +25,12 @@ fds
 
 </head>
 <body>
-    @yield('content')
+    @extends('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @extends('partials.footer')
 </body>
 </html>
