54f9f6d33ec01ba73682e888d92a8414409cb72c
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-21 16:48:28 +0200

    aggiungo partials

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+{{-- Estendo il Layout "Default" in questo file --}}
+@extends('layouts.default')
+
+
+{{-- Creo il titolo per la "home page" di Laravel Comix --}}
+@section('page_title', 'Laravel-Comix')
+
+
+{{-- Creo il contenuto da iniettare nel layout di default --}}
+@section('content')
